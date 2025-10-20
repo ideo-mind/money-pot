856db13ca97c0a1298ccae2bf4696f7b9c1ed7c1
Hiro <laciferin@gmail.com>
2025-10-20 19:23:04 +0530

    prefix all env vars: to VITE_

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly APTOS_NODE_URL: string
+  readonly VITE_APTOS_NODE_URL: string
   readonly VITE_MONEY_POT_CONTRACT_ADDRESS: string
-  readonly MONEY_AUTH_URL: string
+  readonly VITE_MONEY_AUTH_URL: string
   readonly VITE_VERIFIER_SERVICE_BASE_URL: string
   readonly VITE_USDC_TOKEN_ADDRESS: string
 }
